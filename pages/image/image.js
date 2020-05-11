52f6a4b394b4535ce0eefd099507ee8db0e7c8b0
lj2000star <653406168@qq.com>
2020-05-11 10:31:17 +0800

    初始化项目

--- pages/image/image.js
+++ pages/image/image.js
@@ -1,66 +1,59 @@
-// pages/image/image.js
+//index.js
+//获取应用实例
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    motto: '上传鹦鹉自拍',
+    userInfo: {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //事件处理函数
+  uploadImage: function() {
+    var that = this
+    wx.chooseImage({
+  success: function(res) {
+    var tempFilePaths = res.tempFilePaths
+    wx.showToast({
+  title: '鉴定中，请稍候',
+  icon: 'loading',
+  duration: 2000
+})
+    wx.uploadFile({
+      url: 'https://1521861041782710.cn-qingdao.fc.aliyuncs.com/2016-08-15/proxy/animal/bird/', //仅为示例，非真实的接口地址
+      header: {
+      'content-type': 'application/json'
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+      filePath: tempFilePaths[0],
+      name: 'file',
+      success: function(res){
+        console.log(res.data)
+        wx.hideToast()
+        var data = JSON.parse(res.data)
+        if(!data.attributes){
+          that.setData({
+            userInfo:{
+            avatarUrl:data.url,
+            tips:'未检测成功'
+            }
+          })
+          return
+        }
+        // const genders = {
+        //   'Male':'帅哥',
+        //   'Female':'美女'
+        // }
+        // that.setData({
+        //   userInfo:{
+        //     avatarUrl:data.url,
+        //     tips:'一位'+data.attributes.age.value+'岁的'+genders[data.attributes.gender.value]
+        //   }
+        // })
+        //do something
+      }
+    })
+  }
+})
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function () {
+    console.log('onLoad')
   }
 })
--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -1,66 +1,60 @@
-// pages/photo/photo.js
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    userInfo: {},
+    barTitle:[{'name':'收藏',currentTab:0},
+              {'name':'制作',currentTab:1},
+              {'name':'我的',currentTab:2}],
+    items:[
+       {title:'模版1',cover:'http://img.huiyoobao.com/funny/columnback/1474432200020_orign.jpg'},
+       {title:'模版2',cover:'http://img.huiyoobao.com/funny/columnback/1473825600311_orign.jpg'},
+       {title:'模版3',cover:'http://img.huiyoobao.com/funny/columnback/1473825601094_orign.jpg'},
+       {title:'模版4',cover:'http://img.huiyoobao.com/funny/columnback/1473825076410_orign.jpg'},
+       {title:'模版5',cover:'http://img.huiyoobao.com/funny/columnback/1473825075998_orign.jpg'}],
+    winHeight:0,
+    winWidth:0,
+    currentTab:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  swichNav:function(e){
+    this.setData({
+      currentTab:e.currentTarget.dataset.currenttab
+    })
+    // wx.request({
+    //   url: 'https://URL',
+    //   data: {},
+    //   method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+    //   // header: {}, // 设置请求的 header
+    //   success: function(res){
+    //     // success
+    //   },
+    //   fail: function() {
+    //     console.log("查询失败！");
+    //   }
+    // })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onLoad: function () {
+    console.log('onLoad')
+    let that=this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          winHeight:res.windowHeight,
+          winWidth:res.windowWidth
+        })
+      }
+    })
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   }
 })
